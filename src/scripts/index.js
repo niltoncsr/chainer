05cd0590686ed9bee5a326750210c4536686567e
Nilton Cesar <niltoncms@gmail.com>
2017-01-30 10:07:53 -0300

    Add a key-value pair to data through click.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,18 +3,17 @@ var Vue = require('../vendors/vue/dist/vue.js');
 new Vue({
 	el: '#app',
 	data: {
-		d26012017: {
-			work: 'I created a GitHub repo with very useful code snippets used internally'
-		}
+		activities: {}
 	},
 
-	computed: {
-		days: function() {
-			var daysList = [];
-			if(this.d26012017) {
-				daysList.push(this.d26012017)
-			}
-			return daysList;
+	methods: {
+		register: function() {
+			var today = new Date();
+			this.activities[today.getFullYear()] = {
+				[today.getMonth()]: {
+					[today.getDate()]: 'I created a GitHub repo with very useful code snippets used internally'
+				}
+			};
 		}
 	}
 });
