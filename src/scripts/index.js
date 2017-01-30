adbb31ba621cc24ab2f3fc7e607330949da90939
Nilton Cesar <niltoncms@gmail.com>
2017-01-30 14:03:22 -0300

    Fix names of keys for numbers indicating the date.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -19,14 +19,14 @@ new Vue({
 					month = (new Date()).getMonth(),
 					day = (new Date()).getDate();
 
-			if(!this.activities.year)
-				this.activities.year = {}
+			if(!this.activities[year])
+				this.activities[year] = {}
 
-			if(!this.activities.year.month)
-				this.activities.year.month = {}
+			if(!this.activities[year][month])
+				this.activities[year][month] = {}
 
-			if(!this.activities.year.month.day)
-				this.activities.year.month.day = this.description
+			if(!this.activities[year][month][day])
+				this.activities[year][month][day] = this.description
 		}
 	}
 });
