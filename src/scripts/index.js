512e7143844240b9ee62daea5cbf519a3674fda0
Nilton Cesar <niltoncms@gmail.com>
2017-01-29 22:47:40 -0300

    Add a front-end development stack.

---
+++ src/scripts/index.js
@@ -0,0 +1,20 @@
+var Vue = require('../../bower_components/vue/dist/vue.js');
+
+new Vue({
+	el: '#app',
+	data: {
+		d26012017: {
+			work: 'I created a GitHub repo with very useful code snippets used internally'
+		}
+	},
+
+	computed: {
+		days: function() {
+			var daysList = [];
+			if(this.d26012017) {
+				daysList.push(this.d26012017)
+			}
+			return daysList;
+		}
+	}
+});
